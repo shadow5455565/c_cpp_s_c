44957fbf51ef3f4fed836b9e9e7cafcd17ffade1
Raul Radu <shadow5455565@users.noreply.github.com>
2017-03-15 22:32:24 +0100

    Update startup.py

--- libs/startup/startup.py
+++ libs/startup/startup.py
@@ -1,18 +1,23 @@
-import libs.startup.update as update
-import libs.globals as globals
+## import libs.x.class as class where x is a directory
+import libs.startup.updater.updater as updater
+import libs.variables.VBootStrapper.VBootStrapper as VBootStrapper
 class startup:
    def startup():
-      print("Checking for updates...", end=" ")
-      globals.updates= update.update("Check-Updates")
+      print("(*) Checking for updates...", end=" ")
+      VBootStrapper.updates= update.update("Check-Updates")
+      print("  (*)Checking for new BootStrapper version...")
       if globals.updates["BS_New_version"]==1:
-         print("(!) New version ("+ updates["BS-Version"] + ") of the BootStapper!")
-         print("(!) Downloading the new version...", end="")
-         update.update("BS-update") ## in update.update: gitdownload("shadow5455565", "c_cpp_s_c") ## (author, repository name)
-         print("[ DONE ]")
+         print("     (!) New version ("+ VBootStrapper.updates["BS-Version"] + ") of the BootStapper!")
+         print("     (!) Downloading the new version...", end="")
+         updater.update("BS-update") ## in update.update: gitdownload("shadow5455565", "c_cpp_s_c") ## (author, repository name)
+         print(" [ DONE ]")
       else:
-         print("(-) The BootStrapper is already updated.")
+         print("     (-) The BootStrapper is already updated.")
+      print("  (*)Checking for new C Script Analyzer version...")
       if globals.updates["CSA_New_version"]==1:
-         print("(!) New version (" + updates["CSA_version"] + ") of the C Script Analyzer available...")
-         print("(!) Downloading the new version...", end="")
-         update.update("CSA-Update")
-               
+         print("     (!) New version (" + VBootStrapper.updates["CSA_version"] + ") of the C Script Analyzer available...")
+         print("     (!) Downloading the new version...", end="")
+         updater.update("CSA-Update")
+         print(" [ DONE ]")
+      else:
+         print("     (-) The C Script Analyzer is already updated.")
